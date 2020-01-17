fdcba9df8d07a99649d3e2d5109b28c8b5608a4a
JulioPapel <julio.papel@me.com>
2020-01-17 16:02:29 +0100

    Initial gem Skeleton

---
+++ lib/minetest/cli.rb
@@ -0,0 +1,8 @@
+require "minetest/cli/version"
+
+module Minetest
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/minetest/cli/version.rb
@@ -0,0 +1,5 @@
+module Minetest
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
